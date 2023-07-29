fc4d312eea067867c9633afaad215dafee74c90a
WOON-DESKTOP\shufa <shufa17@gmail.com>
2023-07-29 20:35:57 +0800

    import formModule then use ngModule for input tag

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  // data binding. title and developer can be used in html
   title = 'my-first-app';
+  developer = 'shufa';
 }
