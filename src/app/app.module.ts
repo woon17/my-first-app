28cfeb5b9282ad0a579cadacc0e04a9d45136be4
WOON-DESKTOP\shufa <shufa17@gmail.com>
2023-07-29 21:29:44 +0800

    add ServerComponent into angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
+// @NgModule: The @NgModule decorator is used to define an Angular module.
+// It specifies the metadata for the module,
+// including the declarations, imports, exports, providers, and other settings.
 @NgModule({
+  // This property is an array that includes the components, directives, and pipes that belong to this module.
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
+  // This property is an array of other modules that this module depends on.
+  // You can import modules from Angular's standard library or custom modules created in your application.
   imports: [
     BrowserModule
   ],
+  // This property is an array that includes the services that are available in the injector of this module.
+  // Services added here are scoped to the module, meaning each instance of the service is unique within this module's context.
   providers: [],
   // list all the components should be known to angular
   // start from main.ts, pass AppModule as a argument to bootstrapModule
---
+++ src/app/server/server.component.ts
@@ -0,0 +1,17 @@
+
+
+// Component Decorator: Used to define an Angular component.
+// It specifies the metadata for the component, including the selector,
+// template or templateUrl, styles or styleUrls, and other configuration options.
+
+import { Component } from "@angular/core";
+
+// use export as we will use ServerComponent in other place
+@Component({
+  selector: 'app-server',
+  // "./" notation typically represents the current directory
+  templateUrl: './server.component.html'
+})
+export class ServerComponent {
+
+}
