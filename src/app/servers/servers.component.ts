d7ce0996534eb7255618afaff56c6e65ef0450a4
WOON-DESKTOP\shufa <shufa17@gmail.com>
2023-07-29 22:26:39 +0800

    property binding

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -3,13 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   // selector: '[app-servers]',
   selector: '.app-servers',
-  template: `
-  <app-server></app-server>
-  <app-server></app-server>`,
-  // templateUrl: './servers.component.html',
+  // template: `
+  // <app-server></app-server>
+  // <app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 
 })
 export class ServersComponent {
-
+  allowNewServer = false;
+constructor(){
+  setTimeout(() => {this.allowNewServer=true},2000)
+}
 }
