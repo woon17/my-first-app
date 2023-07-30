43fae5416e875d6121286aa13c6c17f458b20702
WOON-DESKTOP\shufa <shufa17@gmail.com>
2023-07-30 14:16:59 +0800

    ngFor directive for multiple server components

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,11 +15,14 @@ export class ServersComponent {
   serverCreationStatus = "No server was created!";
   serverName = 'testServer';
   serverCreated = false;
+  servers = ['TestServer1', 'TestServer2'];
+
   constructor() {
     setTimeout(() => { this.allowNewServer = true }, 2000)
   }
   onCreateServer() {
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
+    this.servers.push(this.serverName);
     this.serverCreated = true;
   }
   onUpdateSeverName(event: Event) {
